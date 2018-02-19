03f53eaf740062e03ea8b0a815dab1ca7e213d6a
nitesh2730 <niteshnitj10@gmail.com>
2018-02-19 11:14:29 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,115 +1,208 @@
 var express = require('express');
-// var data = require('../public/func');
 var router = express.Router();
+var request = require('request');
+var rp = require('request-promise');
 var MongoClient = require('mongodb').MongoClient;
-var fs = require('fs');
-var rslt=null;
 var x=[];
+var y;
+var data1;
 
 
 /* GET users listing. */
 router.get('/getBook',function(req,res){
      console.log("hello");
-     
-
 })
+
+router.post("/selectedData",function(req,res){
+    console.log("selected data is here.........")
+    // var dd=JSON.parse(req.body);
+    var dd=req.body;
+    // console.log(typeof(dd));
+    console.log(typeof(req.body));
+    console.log(req.body);
+    console.log(dd);
+    const LUIS_programmaticKey = "";
+    // ID of your LUIS app to which you want to add an utterance
+    // const LUIS_appId = "";
+    const LUIS_appId = "";
+    // The version number of your LUIS app
+    const LUIS_versionId = "0.1";
+    // data1=[{"text":"today is saturday","intentName":"None","entityLabels":[]},{"text":"this laptop has gone google","intentName":"None","entityLabels":[]}];
+    data1=dd;
+    var trainAfterAdd=true
+    var requestTrainingStatus=true
+    //if i set these variables as true training and status will be performedd
+    // Parse command line arguments:
+    // -train to train based on the utterances in uploadFile
+    // -status to get training status
+    if (process.argv.length >= 3) {
+      //  console.log("value of agrv[0 and 1],")
+      //  console.log(process.argv[0])
+    //console.log(process.argv[1])
+    
+        if (process.argv[2] === "-train") {
+            trainAfterAdd = true;
+        } else if (process.argv[2] === "-status") {
+            requestTrainingStatus = true;
+        }
+    }
+    // Send JSON as the body of the POST request to the API
+    var sendUtteranceToApi = async (options) => {
+        try {
+    
+            var response; 
+            if (options.method === 'POST') {
+                response = await rp.post(options);
+            } else if (options.method === 'GET') {
+                response = await rp.get(options);
+            }
+            
+            return { request: options.body, response: response };
+    
+        } catch (err) {
+            throw err;
+        }
+    }
+    // upload configuration 
+    var configAddUtterance = {
+        LUIS_subscriptionKey: LUIS_programmaticKey,
+        LUIS_appId: LUIS_appId,
+        LUIS_versionId: LUIS_versionId,
+       // inFile: data1,
+        uri: "", LUIS_versionId)
+    };
+    
+    // Call add-utterance
+    var addUtterance = async (config) => {
+        
+            try {
+        
+                // Extract the JSON for the request body
+                // The contents of the file to upload need to be in this format described in the comments above.
+                var jsonUtterance = data1;
+                console.log('value in jsonUtterance')
+                console.log(jsonUtterance)
+                // Add an utterance
+                var utterancePromise = sendUtteranceToApi({
+                    uri: config.uri,
+                    method: 'POST',
+                    headers: {
+                        'Ocp-Apim-Subscription-Key': config.LUIS_subscriptionKey
+                    },
+                    json: true,
+                    body: jsonUtterance
+                });
+        
+                let results = await utterancePromise;
+             //   let response = await fse.writeJson(config.inFile.replace('.json', '.results.json'), results);
+        
+                console.log("Add utterance done");
+        
+            } catch (err) {
+                console.log(`Error adding utterance:  ${err.message} `);
+                //throw err;
+            }
+        
+        }
+        // training configuration 
+    var configTrain = {
+        LUIS_subscriptionKey: LUIS_programmaticKey,
+        LUIS_appId: LUIS_appId,
+        LUIS_versionId: LUIS_versionId,
+        uri: "", LUIS_versionId),
+        method: 'POST', // POST to request training, GET to get training status
+    };
+        // Call train
+    var train = async (config) => {
+        
+            try {
+        
+                var trainingPromise = sendUtteranceToApi({
+                    uri: config.uri,
+                    method: config.method, // Use POST to request training, GET to get training status 
+                    headers: {
+                        'Ocp-Apim-Subscription-Key': config.LUIS_subscriptionKey
+                    },
+                    json: true,
+                    body: null      // The body can be empty for a training request
+                });
+        
+                let results = await trainingPromise;
+                
+                if (config.method === 'POST') {
+               //     let response = await fse.writeJson(path.join(__dirname, 'training-results.json'), results);        
+                    console.log(`Training request sent. The status of the training request is: ${results.response.status}.`);
+                } else if (config.method === 'GET') {
+                 //   let response = await fse.writeJson(path.join(__dirname, 'training-status-results.json'), results);
+                  //  console.log(`Training status saved to file. `);
+                }
+                
+            } catch (err) {
+                console.log(`Error in Training:  ${err.message} `);
+                // throw err;
+            }
+        
+        }
+        // MAIN
+    if (trainAfterAdd) {
+        // Add the utterance to the LUIS app and train
+        addUtterance(configAddUtterance)
+            .then(() => {
+                console.log("Add utterance complete. About to request training.");
+                configTrain.method = 'POST';
+                return train(configTrain, false);
+            }).then(() => {
+                console.log("Training process complete. Requesting training status.");
+                configTrain.method = 'GET';
+                return train(configTrain, true);
+            }).then(() => {
+                console.log("process done");
+            });
+    } else if (requestTrainingStatus) {
+        // Get the training status
+        configTrain.method = 'GET';
+        train(configTrain)
+            .then(() => {
+                console.log("Requested training status.");
+            });
+    }
+    else {
+        // Add the utterance to the LUIS app without training it afterwards
+        addUtterance(configAddUtterance)
+            .then(() => {
+                console.log("Add utterance complete.");
+            });
+    
+    }
+  })
 router.post("/sendBooks",function(req,res){
     var sdate=String(req.body.startdate);
     var edate=String(req.body.enddate);
-    // sdate="'"+sdate+"'";
-    // edate="'"+edate+"'";
-    console.log(sdate,edate);
-    // var sdate="2017-12-12T10:31:47.31Z";
-    // var edate="2017-12-30T10:31:47.31Z";
-        var fileload;
-     var url='';
-    console.log("outside mongo connect");
-  
+    // console.log(sdate,edate);
+    var url='';
+    
     MongoClient.connect(url, function(err, db) {
         if (err){
             console.log("error inside MongoClient if: ",err);
             throw err;
         }
-            
-        // var sdate=start;//='2017-12-11 00:00:00';
-        // var edate=end;//='2017-12-13 00:00:00';    
-        // console.log(db);   
-        const myAwesomeDB = db.db('admin');  
-        var anotherobj;
-        var myobj;
-        var newobj;
-        // var x="";
-        console.log("inside mongo connect");
-        // console.log(sdate,edate);
-        /*
-        const myAwesomeDB = db.db('handoffdb')
-        myAwesomeDB.collection("results").findOne({}, function(err, result) {
-            if (err){
-                console.log(err);
-                throw err;
-            } 
-        console.log(result);
-        db.close();
-        });
-        */
-        // console.log(db);
-        
-      
-       /* myAwesomeDB.collection("results").find({text : "computer black screen"}).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            rslt=result;
-            db.close();
-          });*/
-          var count=0;
-        //   {DateAdded:{$gte:"2017-12-9", $lt:"2017-12-30"}},{_id: false,DateAdded:false}
+        const myAwesomeDB = db.db('admin');
+        var count=0;
         myAwesomeDB.collection("results").find({DateAdded:{$gte:sdate, $lt:edate}},{_id: false,DateAdded:false}).toArray(function(err, results) {
-            console.log("inside query");
             if (err) 
                 throw err;
-                // console.log(results);
             l=results.length;  // we want to fetch the last file from the colletions
-              console.log("length of results",l);
             for(i=0;i<l;i++){
                 ln=(results[i].conversations.length);
                 count=count+ln;
-                console.log("length of results.conversations",ln);
                 for(j=0;j<ln;j++){
                     x.push(results[i].conversations[j]);
-                    console.log("-------------------------------------------------------")
-                    console.log(results[i].conversations[j]);
-                    console.log("-------------------------------------------------------")
                 }
-                // y = results[i].conversations;
-                // console.log('----------------------------------------------datad----------')
-                // console.log(JSON.parse(y));
-                // console.log('----------------------------------------------datad----------')
             }
-            // console.log(x);   ///this contains all the content of fetched files combined that is needed to be stored
-            console.log("NO. of records fetched= %d",l);
-            // res.json(x);
-            console.log("-------------------------------------------------end");
-            console.log(typeof(x));
-            console.log(x);
-            console.log(x.length);
-            console.log(count);
             res.json(x);
             db.close();
-        });
-        
+        });        
     });
-
-//,4000);
- /*   if(rslt){
-        console.log(rslt);
-    }
-    else{
-        console.log("---------");
-    }
-    */  
-    
 })
-
-//useful link   https://www.w3schools.com/jsref/jsref_toisostring.asp
 module.exports = router;
 
