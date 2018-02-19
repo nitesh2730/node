ec6dc91709239b7e3084ba5127060bc77ad4870d
nitesh2730 <niteshnitj10@gmail.com>
2018-02-19 11:09:51 +0530

    Update func.js

--- public/func.js
+++ public/func.js
@@ -1,11 +1,70 @@
-
-console.log("yuu");
 var startdate;
 var enddate;
 var x;
+var myBooks;
+var arr=[];
+//            function ckeck(){
+//        console.log("jjjjjjjjjjjjjjjjjjjjjj")
+//      }  
+    
+        function dump(){
+                     console.log("hello there");
+                        var checkedValue=null ; 
+        
+     
+        for(var i=0; i<myBooks.length; ++i){
+       var inputElements = document.getElementsByName('name');
+    //  console.log(inputElements[i]);
+      if(inputElements[i].checked){
+           checkedValue = inputElements[i].value;
+           var res = (checkedValue + "").split("*");
+           console.log(res);
+                 
+ // new code added here.....................................................
+                 
+           var text=res[0];
+           var intentName =res[1];
+           var entityLabels=[];
+        var obj={text,intentName,entityLabels};
+           arr.push(obj);
+   //............................................................................      
+        //    for(var j=0;j<res.length;j++){
+        //        selectedData.text=res[j];
+        //        selectedData.intentName=res[]
 
+
+        //    }
+        //   selectedData.text=res[0];
+        // selectedData.intentName=res[1];
+
+
+          
+      }
+     
+}
+/* ---------------------------------------------------------------------------- */
+ var data=JSON.stringify(arr);
+ var http = new XMLHttpRequest();
+        var url = "users/selectedData";
+        ;
+        //var params=startdate;
+        http.open("POST", url, true);
+
+        //Send the proper header information along with the request
+        http.setRequestHeader("Content-type", "application/json");
+
+         http.onreadystatechange = function() {
+              if(http.readyState == 4 && http.status == 200){
+
+              }
+         }
+         http.send(data);
+         
+   /*-----------------------------------------------------------------*/     
+
+     }
+ 
      function startendbtn(){
-         console.log("gadha");
          startdate = document.getElementById('search51startdate').value;
         enddate= document.getElementById('search51enddate').value;
         console.log("startdate" + startdate);
@@ -24,154 +83,88 @@ var x;
         http.onreadystatechange = function() {//Call a function when the state changes.
             if(http.readyState == 4 && http.status == 200) {
                 x=http.responseText;
-                var myBooks=JSON.parse(x);
+                myBooks=JSON.parse(x);
+                console.log(myBooks);
                 var col = [];
-                for (var i = 0; i < myBooks.length; i++) {
-                    for (var key in myBooks[i]) {
-                        console.log(key);
-                        if (col.indexOf(key) === -1) {
-                            col.push(key);
-                        }
-                    }
-                }
-                col.push("Select");
+                // for (var i = 0; i < myBooks.length; i++) {
+                //     for (var key in myBooks[i]) {
+                //         console.log(key);
+                //         if (col.indexOf(key) === -1) {
+                //             col.push(key);
+                //         }
+                //     }
+                // }
+                // col.push("Select");
                 
                 // CREATE DYNAMIC TABLE.
+                var tablearea;
+                var table;
+                var thead;
+                var tr;
+                var th;
+                 tablearea = document.getElementById('ShowDataID');
+                 table = document.createElement('table');
+                 thead = document.createElement('thead');
+                  tr = document.createElement('tr');
+            //       var x = document.createElement('input');
+            //     x.setAttribute('type', 'checkbox');
+            //   x.setAttribute('name', 'city');
+            //    x.setAttribute('value', 'London');
+            //      x.setAttribute('id', 1);
+            // var box = document.createElement('input');
+            //   box.type = "button";
+            //    box.name = "name";
+            //    box.value = "value";
+            //    box.id = "id";
+            var data =["text","intentName","entityLabels","select"];
+
                 var table = document.createElement("table");
-                
-                table.setAttribute("style", "overflow-y: auto; width = 75% ; height = 75%, border-collapse: collapse;");
-                
-                // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-                
-                var tr = table.insertRow(-1);                   // TABLE ROW.
-                
-                for (var i = 0; i < col.length; i++) {
-                    var th = document.createElement("th");      // TABLE HEADER.
-                    th.innerHTML = col[i];
-                    tr.appendChild(th);
-                    // if(col[])
-                }
-                
-                // ADD JSON DATA TO THE TABLE AS ROWS.
-                for (var i = 0; i < myBooks.length; i++) {
-                
-                    tr = table.insertRow(-1);
+                var tablearea = document.getElementById("showData");
+                  for (var i = 0; i < 4; i++) {
+                   var headerTxt = document.createTextNode(data[i]);
+                   th = document.createElement('th');
+                       th.appendChild(headerTxt);
+                        tr.appendChild(th);
+                     thead.appendChild(tr);
+                  table.appendChild(thead);}
 
-                   
+    for (var i = 0; i < myBooks.length; i++) {
+          var box = document.createElement('input');
+            box.type = "checkbox";
+               box.name = "name";
+               box.class="checking";
+               box.value = myBooks[i].text +"*"+myBooks[i].intentName ;
+               box.id = myBooks[i];
+              //box.setAttribute("checked",false);
+               box.onclick='check();';
                     
-                    for (var j = 0; j < col.length; j++) {
-                        var tabCell = tr.insertCell(-1);
-                        tabCell.innerHTML = myBooks[i][col[j]];
-                        if(j==3){
-                            var checkbox = "<input type = 'checkbox' name = 'name1' value = 'checked'/>";
-                           // checkbox.type = "checkbox";
-                           // checkbox.name = "name";
-                           // checkbox.value = "value";
-                           // checkbox.id = "id";
-                            //document.getElementById().checked = true;
-                           
-                            
-                            tabCell.innerHTML = checkbox;
-                        }
-                    }
-                }
-                
-                // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-                var divContainer = document.getElementById("showData");
-                divContainer.innerHTML = "";
-                divContainer.appendChild(table);
-                var divX = document.getElementById("btn123");
-                // var btr = "<button  class=btn style='margin-top:10px; type = 'submit'>Submit</button>";
-                // var trr = document.createElement("tr");
-                // divX.innerHTML = btr;
+   
 
+        tr = document.createElement('tr');
+        tr.appendChild(document.createElement('td'));
+        tr.appendChild(document.createElement('td'));
+        tr.appendChild(document.createElement('td'));
+          tr.appendChild(document.createElement('td'));
+        tr.cells[0].appendChild(document.createTextNode(myBooks[i].text));
+        tr.cells[1].appendChild(document.createTextNode(myBooks[i].intentName));
+    tr.cells[2].appendChild(document.createTextNode(myBooks[i].entityLabels));
+    tr.cells[3].appendChild(box);
+
+        table.appendChild(tr);
+    }
+    tablearea.appendChild(table);
+                  var divX = document.getElementById("btn123");
+                var btr = "<button  class=btn style='margin-top:10px; type = 'submit'><span class='text'' onclick='dump();'>Submit</span></button>";
+                var trr = document.createElement("tr");
+                divX.innerHTML = btr;   
+
+                 
+  
 
             }
 
 }
 http.send(data);
-// var myBooks=JSON.stringify(x);
-// var col = [];
-// for (var i = 0; i < myBooks.length; i++) {
-//     for (var key in myBooks[i]) {
-//         console.log(key);
-//         if (col.indexOf(key) === -1) {
-//             col.push(key);
-//         }
-//     }
-// }
 
-// // CREATE DYNAMIC TABLE.
-// var table = document.createElement("table");
 
-// // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-// var tr = table.insertRow(-1);                   // TABLE ROW.
-
-// for (var i = 0; i < col.length; i++) {
-//     var th = document.createElement("th");      // TABLE HEADER.
-//     th.innerHTML = col[i];
-//     tr.appendChild(th);
-// }
-
-// // ADD JSON DATA TO THE TABLE AS ROWS.
-// for (var i = 0; i < myBooks.length; i++) {
-
-//     tr = table.insertRow(-1);
-
-//     for (var j = 0; j < col.length; j++) {
-//         var tabCell = tr.insertCell(-1);
-//         tabCell.innerHTML = myBooks[i][col[j]];
-//     }
-// }
-
-// // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-// var divContainer = document.getElementById("showData");
-// divContainer.innerHTML = "";
-// divContainer.appendChild(table);
 }
-
-
-// module.exports= {
-// startdate:"2017-12-12T10:31:47.31Z",
-// enddate:"2017-12-30T10:31:47.31Z"
-// };
-        
-//         var app = angular.module('myApp', []);
-// app.controller('myController',
-//     function ($scope, $http) {
-
-//         var request = {
-//             method: 'post',
-//             url: 'users/sendBooks',
-//             dataType: 'json',
-//             data:{
-//                 "start":startdate,
-//                 "end":enddate
-//             },
-//             headers:{'Content-Type': 'application/x-www-form-urlencoded'}
-           
-//         };
-//         $http(request)
-//             .success(function (jsonData) {
-//                 $scope.myData = jsonData;
-//             })
-//             .error(function () {
-//               console.log("Error here");
-
-//             });
-
-
-
-    //     alert(startdate.value);
-    // 	alert(enddate.value);
-    // }
-
-    // var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.onreadystatechange = function() { 
-    //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    //         callback(xmlHttp.responseText);
-    // }
-    // xmlHttp.open("", theUrl, true); // true for asynchronous 
-    // xmlHttp.send(null);
-   
